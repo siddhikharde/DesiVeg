6a26a05dcd9ba8faed6c3b2ef77d224744d9d320
siddhikharde <siddhikharde2005@gmail.com>
2026-01-02 21:33:00 +0530

    form creation done

--- src/views/CallUs.jsx
+++ src/views/CallUs.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import Button from '../components/Button'
+import toast,{Toaster} from 'react-hot-toast'
 const ContactCard=({type,detail,onClick})=>{
   return(
     <div className='bg-white p-5 cursor-pointer rounded-lg shadow-md md:w-80 w-[100%] flex flex-col gap-2 items-center' onClick={onClick}>
@@ -25,9 +27,24 @@ function CallUs() {
         <ContactCard type={"Email"} detail={"desiveg123@gmail.com"} onClick={()=>{
             window.location.href="mailto:desiveg123@gmail.com"
         }}/>
-        
-
     </div>
+    <div className='flex mx-auto flex-col justify-center items-center gap-5 border border-[#2e8434] w-[300px] p-5 rounded-2xl'>
+        <h3 className='font-bold text-[#1B5E20] text-2xl'>Send us a Message</h3>
+        <input type="text" required placeholder='Enter a Name' className='border   focus:outline-none w-full border bodrder-[#2e8434 ] p-2 rounded-xl'/>
+        <input type="email" required placeholder='Enter a Email' className='border  focus:outline-none w-full border bodrder-[#2e8434 ] p-2 rounded-xl'/>
+        <textarea placeholder='Enter your Message' required className='border p-2   focus:outline-none w-full border bodrder-[#2e8434 ] rounded-xl'></textarea>
+        <Button title={"Submit"} variant='PRIMARY' size='md' onClick={()=>{
+            if(document.querySelector('input[required]').value && document.querySelector('textarea[required]').value){
+            toast.success("Message sent successfully!");
+            document.querySelector('input[required]').value="";
+            document.querySelectorAll('input[required]')[1].value="";
+            document.querySelector('textarea[required]').value="";
+            }else{
+                toast.error("Please fill all the fields");
+            }
+        }}/>   
+    </div>
+    <Toaster/>
     </div>
     <Footer/>
    </div>
