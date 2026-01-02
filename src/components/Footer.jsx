9388dbf321200eae5c3527e719d959aa007c90a0
siddhikharde <siddhikharde2005@gmail.com>
2026-01-02 15:40:08 +0530

    links config added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,20 @@
 import React from 'react'
 import { Github,Youtube,Instagram } from 'lucide-react';
+const SOCIAL_LINKS=[{
+    name:"GitHub",
+    url:"https://github.com/siddhikharde/DesiVeg.git",
+    icon:<Github size={20}/>
+},
+{
+    name:"YouTube",
+    url:"https://www.youtube.com/",
+    icon:<Youtube size={20}/>
+},
+{
+    name:"Instagram",
+    url:"https://www.instagram.com/",
+    icon:<Instagram size={20}/>
+  }];
 const FooterLink=({name ,url,icon})=>{
   return(
     <a href={url} className='text-white flex items-center gap-2'>
@@ -9,19 +24,22 @@ const FooterLink=({name ,url,icon})=>{
 }
 function Footer() {
   return (
-    <div className='w-full  bg-[#2E7D32] '>
-      <div className='flex items-center justify-around mt-10'>
+    <div className='w-full  bg-[#2E7D32]  '>
+      <div className='flex items-center justify-around mt-10 border-b border-[#173e19] pb-2 flex-col md:flex-row gap-5'>
         <div className='text-center text-white'>
         Made with ❤️ by DesiVeg
       </div>
-      <div className='flex flex-col items-center justify-center gap-2 my-3 text-white'>
-        <FooterLink name="GitHub" url="https://github.com/siddhikharde/DesiVeg.git" icon={<Github />} />
-        <FooterLink name="YouTube" url="https://www.youtube.com/" icon={<Youtube />} />
-        <FooterLink name="Instagram" url="https://www.instagram.com/" icon={<Instagram />} />
+      <div className='flex flex-col items-start justify-center gap-2 my-3 text-white'>
+        {SOCIAL_LINKS.map((link,index)=>{
+          const {name,url,icon}=link;
+          return(
+          <FooterLink key={index} name={name} url={url} icon={icon}/>
+          )
+        })}
       </div>
       
       </div>
-      <footer className='bg-[#184c1b] text-white text-center p-4  '>
+      <footer className='bg-[#2E7D32] text-white text-center p-4  '>
         <p>&copy; 2024 DesiVeg. All rights reserved.</p>
       </footer>
     </div>
