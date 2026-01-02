424c950db591618230ff5d08be4692e4de279f21
siddhikharde <siddhikharde2005@gmail.com>
2026-01-02 15:28:33 +0530

    footerlink component created

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import { Github,Youtube,Instagram } from 'lucide-react';
-
+const FooterLink=({name ,url,icon})=>{
+  return(
+    <a href={url} className='text-white flex items-center gap-2'>
+      {icon} {name}
+    </a>
+  )
+}
 function Footer() {
   return (
     <div className='w-full  bg-[#2E7D32] '>
@@ -8,13 +14,14 @@ function Footer() {
         <div className='text-center text-white'>
         Made with ❤️ by DesiVeg
       </div>
-      <div className='flex items-center justify-center gap-5 my-3 text-white'>
-        <a href="https://github.com/siddhikharde/DesiVeg.git"><Github /></a>
-        <a href="https://www.youtube.com/"><Youtube /></a>
-        <a href="https://www.instagram.com/"><Instagram /></a>
+      <div className='flex flex-col items-center justify-center gap-2 my-3 text-white'>
+        <FooterLink name="GitHub" url="https://github.com/siddhikharde/DesiVeg.git" icon={<Github />} />
+        <FooterLink name="YouTube" url="https://www.youtube.com/" icon={<Youtube />} />
+        <FooterLink name="Instagram" url="https://www.instagram.com/" icon={<Instagram />} />
       </div>
+      
       </div>
-      <footer className='bg-[#2E7D32] text-white text-center p-4  '>
+      <footer className='bg-[#184c1b] text-white text-center p-4  '>
         <p>&copy; 2024 DesiVeg. All rights reserved.</p>
       </footer>
     </div>
